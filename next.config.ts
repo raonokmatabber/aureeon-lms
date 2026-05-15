8bd0fb28630524b4acebb496ba74983a522adcd7
Raonok Matabber <raonok.matabber@gmail.com>
2026-05-15 07:20:23 +0000

    fix: next config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
-
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    typedRoutes: false,
+  },
 };
-
 export default nextConfig;
